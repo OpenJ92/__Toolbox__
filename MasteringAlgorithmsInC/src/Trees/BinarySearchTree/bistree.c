2918c0f2c68a65667445029953639f3513dca938
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-07-20 12:36:07 -0400

    walk through rotation implementation. Honestly quite confused by this given the code. Look to draw out the instructions to clarify.

---
+++ MasteringAlgorithmsInC/src/Trees/BinarySearchTree/bistree.c
@@ -0,0 +1,109 @@
+#include "bistree.h"
+
+
+void bistree_init(BisTree* tree, 
+		int (*compare)(const void* key1, const void* key2),
+		void (*destroy)(void* data))
+{
+	bitree_init(tree, destroy);
+	tree->compare = compare;
+	return;
+}
+
+// Wow! This is very complicated. Come back and spend some time drawing 
+// out this implementation of rotation with Lx configuration.
+static void rotate_left(BiTreeNode** node)
+{
+	BiTreeNode* left; BiTreeNode* grandchild;
+	left = bitree_left(*node);
+
+	if (((AvlNode*)bitree_data(left))->factor == AVL_LFT_HEAVY)
+	{
+		// LL rotation
+		bitree_left(*node) = bitree_right(left);
+		bitree_right(left) = *node;
+		((AvlNode*)bitree_data(*node))->factor = AVL_BALANCED;
+		((AvlNode*)bitree_data(left))->factor = AVL_BALANCED;
+		*node = left;
+	}
+	else
+	{
+		//LR rotation
+		grandchild = bitree_right(left);
+		bitree_right(left) = bitree_left(grandchild);
+		bitree_left(grandchild) = left;
+		bitree_left(*node) = bitree_right(grandchild);
+		bitree_right(grandchild) = *node;
+
+		switch (((AvlNode*)(bitree_data(grandchild)))->factor)
+		{
+			case AVL_LFT_HEAVY:
+				((AvlNode*)bitree_data(*node))->factor = AVL_RGT_HEAVY;
+				((AvlNode*)bitree_data(left))->factor = AVL_BALANCED;
+				break;
+			case AVL_BALANCED:
+				((AvlNode*)bitree_data(*node))->factor = AVL_BALANCED;
+				((AvlNode*)bitree_data(left))->factor = AVL_BALANCED;
+				break;
+				
+			case AVL_RGT_HEAVY:
+				((AvlNode*)bitree_data(*node))->factor = AVL_BALANCED;
+				((AvlNode*)bitree_data(left))->factor = AVL_LFT_HEAVY;
+				break;
+		}
+
+		((AvlNode*)bitree_data(grandchild))->factor = AVL_BALANCED;
+		*node = grandchild;
+	}
+
+	return;
+}
+
+// Wow! This is very complicated. Come back and spend some time drawing 
+// out this implementation of rotation with Rx configuration.
+static void rotate_right(BiTreeNode** node)
+{
+	BiTreeNode* right;
+	BiTreeNode* grandchild;
+
+	right = bitree_right(*node);
+
+	if (((AvlNode*)bitree_data(right))->factor == AVL_RGT_HEAVY)
+	{
+		// RR rotation
+		bitree_right(*node) = bitree_left(right);
+		bitree_left(right) = *node;
+		((AvlNode*)bitree_data(*node))->factor = 0;
+		((AvlNode*)bitree_data(right))->factor = 0;
+		*node = right;
+	}
+	else
+	{
+		// RL rotation
+		grandchild = bitree_left(right);
+		bitree_left(right) = bitree_right(grandchild);
+		bitree_right(grandchild) = right;
+		bitree_right(*node) = bitree_left(grandchild);
+		bitree_left(grandchild) = *node;
+
+		switch (((AvlNode*)bitree_data(grandchild))->factor)
+		{
+			case AVL_LFT_HEAVY:
+				((AvlNode*)bitree_data(*node))->factor = AVL_BALANCED;
+				((AvlNode*)bitree_data(right))->factor = AVL_RGT_HEAVY;
+				break;
+			case AVL_BALANCED:
+				((AvlNode*)bitree_data(*node))->factor = AVL_BALANCED;
+				((AvlNode*)bitree_data(right))->factor = AVL_BALANCED;
+				break;
+			case AVL_RGT_HEAVY:
+				((AvlNode*)bitree_data(*node))->factor = AVL_LFT_HEAVY;
+				((AvlNode*)bitree_data(right))->factor = AVL_BALANCED;
+				break;
+		}
+
+		((AvlNode*)bitree_data(grandchild))->factor = AVL_BALANCED;
+		*node = grandchild;
+	}
+	return;
+}
