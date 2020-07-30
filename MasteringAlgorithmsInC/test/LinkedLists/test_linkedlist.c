cc519eab138bf75a9623e49d00696b464a064026
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-07-29 21:23:30 -0400

    work on understanding the use of dynamic pointers

--- MasteringAlgorithmsInC/test/LinkedLists/test_linkedlist.c
+++ MasteringAlgorithmsInC/test/LinkedLists/test_linkedlist.c
@@ -1,5 +1,7 @@
 #include "test_linkedlist.h"
 
+#include <stdio.h>
+
 #define SAMPLES 3 
 
 int init(List** lists)
@@ -7,9 +9,9 @@ int init(List** lists)
 	if ((lists = (List**)malloc(SAMPLES*sizeof(List*))) == NULL){ return -1;}
 	for (int sample = 0; sample < SAMPLES; sample++) 
 	{ 
-		List* ptr = (*lists + sample);
-		if((ptr = (List*)malloc(sizeof(List))) == NULL) { return -1; }
-		list_init(ptr, NULL); 
+		lists[sample] = (List*)malloc(sizeof(List));
+		list_init(lists[sample], NULL); 
+		printf("%p\n", lists[sample]);
 	}
 	return 0;
 }
@@ -22,13 +24,17 @@ int create_empty_list(List* list)
 int create_singlton_list(List* list, const void* data, void (*destroy)(void* data))
 {
 	list->destroy = destroy; int retval;
-	if ((retval = list_insert_next(list, NULL, data)) == -1){ return retval; }
+	if ((retval = list_insert_next(list, NULL, data)) == -1){ return -1; }
 	return 0;
 }
 
 int create_many_list(List* list, const void** data, int data_size, void (*destroy)(void* data))
 {
-	list->destroy = destroy;
+	list->destroy = destroy; int retval;
+	for (int i = 0; i < data_size; i++)
+	{
+		if ((retval = list_insert_next(list, NULL, (*data + i))) == -1){ return -1 ; }
+	}
 	return 0;
 }
 
@@ -40,6 +46,6 @@ void cleanup(List** lists)
 void linkedlist_run_all(void)
 {
 	List** lists = NULL; init(lists);
-	EQ(10, 11 - 1, "linked list insert failed!");
+	int data[10] = {0,1,2,3,4,5,6,7,8,9};
 	return;
 }
