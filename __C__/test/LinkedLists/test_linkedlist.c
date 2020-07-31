6a9dd936490ec5eb2598a5bc127e474e64884f93
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-07-31 14:20:03 -0400

    Write template tests and complete t_list_init

--- __C__/test/LinkedLists/test_linkedlist.c
+++ __C__/test/LinkedLists/test_linkedlist.c
@@ -2,31 +2,78 @@
 
 #include <stdio.h>
 
+#define SAMPLES 3
+
 // setup lists
 void setup_tests();
-List** setup(void) { return NULL; }
+List** setup(void) 
+{ 
+	List** lists; 
+	if ((lists = (List**)malloc(SAMPLES*sizeof(List*))) == NULL)
+	{ 
+		return NULL; 
+	}
 
-void test_list_init(List* list, void (*destroy)(void* data))
-{
-	list_init(list, destroy);
-	EQ(list->size, 1, "");
-	EQ(list->destroy, destroy,  "");
-	EQ(list->head, NULL,  "");
-	EQ(list->tail, NULL, "");
+	for (int sample = 0; sample < SAMPLES; sample++)
+	{
+		if ((lists[sample] = (List*)malloc(sizeof(List)) == NULL))
+		{
+			return NULL;
+		}
+	}
+
+	return lists; 
 }
 
-void test_list_insert_next(List* list);
-void test_list_remove_next(List* list);
-void test_list_destroy(List* list);
+void test_list_init(void (*destroy)(void* data))
+{
+	Lists** lists = setup();
+
+	for (int sample = 0; sample < SAMPLES; sample++)
+	{
+		List* list = lists[sample]
+		list_init(list, destroy);
+		EQ(list->size, 1, "");
+		EQ(list->destroy, destroy,  "");
+		EQ(list->head, NULL,  "");
+		EQ(list->tail, NULL, "");
+	}
+
+	teardown(lists)
+}
+
+void test_list_insert_next(void)
+{
+	Lists** lists = setup();
+	teardown(lists)
+}
+
+void test_list_remove_next(void);
+{
+	Lists** lists = setup();
+	teardown(lists)
+}
+void test_list_destroy(void);
+{
+	Lists** lists = setup();
+	teardown(lists)
+}
 
 void test_linkedlist_run_all(void)
 {
-	List list; 
-	test_list_init(&list, NULL);
+	test_list_init(NULL);
 }
 
 // destroy lists
-void teardown(List**);
+void teardown(List** lists)
+{
+	for (int sample = 0; sample < SAMPLES; sample++)
+	{
+		list_destroy(lists[sample]);
+		free(lists[sample]);
+	}
+	free(lists); return;
+}
 void teardown_tests();
 
 int main() 
