7380b09ab81cc4be1b5f7ac589e8affdca4a74eb
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-07-30 22:54:00 -0400

    Update make file with remaining data structures

--- MasteringAlgorithmsInC/main.c
+++ MasteringAlgorithmsInC/main.c
@@ -1,1 +1,9 @@
-int main(int argc, char* argv[]) { return 0; }
+#include "src/LinkedLists/LinkedList/linkedlist.h"
+#include "src/LinkedLists/CircularSinglyLinkedList/cirularsinglylinkedlist.h"
+
+int main(int argc, char* argv[]) 
+{ 
+	CList clist; clist_init(&clist, NULL); clist_destroy(&clist);
+	List list; list_init(&list, NULL); list_destroy(&list);
+	return 0; 
+}
