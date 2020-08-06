9ab7ab60b97d90ba98290bc5a04182dc0cbe16e7
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-08-06 08:01:57 -0400

    Clear package. Start Purely functional data structures.

--- __Haskell__/ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for PurelyFunctionalDataStructures
-
-## Unreleased changes
---
+++ __Haskell__/src/BatchedQueue.hs
@@ -0,0 +1,20 @@
+module BatchedQueue (BatchedQueue) where
+  import Prelude hiding (head, tail)
+  import Queue
+
+  data BatchedQueue a = BQ [a] [a]
+
+  check [] r = BQ (reverse r) []
+  check f  r = BQ f r
+
+  instance Queue BatchedQueue where
+    empty = BQ [] []
+    isEmpty (BQ f r) = null f
+
+    snoc (BQ f r) x = check f (x : r)
+
+    head (BQ [] _) = error "empty queue"
+    head (BQ (x:f) _) = x
+
+    tail (BQ [] _) = error "empty queue" 
+    tail (BQ (x:f) r) = check f r
