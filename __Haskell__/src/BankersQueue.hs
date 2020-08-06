ede6f7f5618f16a2b0d6a1d9e2acbfff8e9bc384
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-08-06 08:39:33 -0400

    Introduce Instance Sigs and Implement Bankers queue

---
+++ __Haskell__/src/BankersQueue.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE InstanceSigs #-}
+module BankersQueue (BankersQueue) where
+  import Prelude hiding (head, tail)
+  import Queue
+
+  data BankersQueue a = BQ Int [a] Int [a]
+
+  check :: Int -> [a] -> Int -> [a] -> BankersQueue a
+  check lenf f lenr r | lenf >= lenr = BQ lenf f lenr r
+                      | otherwise = BQ (lenf + lenr) (f ++ reverse r) 0 [] 
+
+  instance Queue BankersQueue where
+    empty :: BankersQueue a
+    empty = BQ 0 [] 0 []
+
+    isEmpty :: BankersQueue a -> Bool
+    isEmpty (BQ lenf f lenr r) = (lenf == 0)
+
+    snoc :: BankersQueue a -> a -> BankersQueue a
+    snoc (BQ lenf f lenr r) x = BQ lenf f (lenr + 1) (x : r)
+
+    head :: BankersQueue a -> a
+    head (BQ lenf [] lenr r) = error "empty queue"
+    head (BQ lenf (x:f) lenr r) = x
+
+    tail :: BankersQueue a -> BankersQueue a
+    tail (BQ lenf [] lenr r) = error "empty queue"
+    tail (BQ lenf (x:f) lenr r) = BQ (lenf - 1) f lenr r
--- __Haskell__/src/BatchedQueue.hs
+++ __Haskell__/src/BatchedQueue.hs
@@ -1,20 +1,28 @@
+{-# LANGUAGE InstanceSigs #-}
 module BatchedQueue (BatchedQueue) where
   import Prelude hiding (head, tail)
   import Queue
 
   data BatchedQueue a = BQ [a] [a]
 
+  check :: [a] -> [a] -> BatchedQueue a
   check [] r = BQ (reverse r) []
   check f  r = BQ f r
 
   instance Queue BatchedQueue where
+    empty :: BatchedQueue a
     empty = BQ [] []
+    
+    isEmpty :: BatchedQueue a -> Bool
     isEmpty (BQ f r) = null f
 
+    snoc :: BatchedQueue a -> a -> BatchedQueue a
     snoc (BQ f r) x = check f (x : r)
 
+    head :: BatchedQueue a -> a
     head (BQ [] _) = error "empty queue"
     head (BQ (x:f) _) = x
 
+    tail :: BatchedQueue a -> BatchedQueue a
     tail (BQ [] _) = error "empty queue" 
     tail (BQ (x:f) r) = check f r
