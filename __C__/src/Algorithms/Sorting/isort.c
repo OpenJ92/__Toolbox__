9384e5602c5cb222e4e339a9dd8d3c1bec93be7b
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-08-23 21:51:33 -0400

    finish implementation of insertion sort.

--- __C__/src/Algorithms/Sorting/isort.c
+++ __C__/src/Algorithms/Sorting/isort.c
@@ -6,21 +6,19 @@
 
 int issort(void* data, int size, int esize, int (*compare)(const void * key1, const void * key2))
 {
-	char * a;
-
+	char * a = (char *)data;
 	void * key;
-	int i, j;
 
 	if ((key = malloc(esize)) = NULL) { return -1; }
 
-	for (j = 1; j < size; j++)
+	for (int j = 1; j < size; j++)
 	{
 		memcpy(key, (a + esize * j), esize);
-		i = j - 1;
+		int i = j - 1;
 
 		while (i >= 0 && compare((a + esize*i), key) > 0)
 		{
-			memcpy((a + (i + 1)esize), (a + i * esize), esize);
+			memcpy((a + (i + 1) * esize), (a + i * esize), esize);
 			i--;
 		}
 		
